d24b5722c6d9365502a7ed58ed0de618607e6f36
Pieter <pieter_delamar@hotmail.com>
2023-04-14 14:33:08 +0200

    reviews en producten gesplitst.

---
+++ SplitReviewsAndProducts.py
@@ -0,0 +1,36 @@
+import csv
+
+# define input and output file paths
+input_file = "combined_with_colors.csv"
+product_output_file = "products.csv"
+review_output_file = "reviews.csv"
+
+# open input and output files
+with open(input_file, "r", newline="", encoding="utf-8") as infile, \
+     open(product_output_file, "w", newline="", encoding="utf-8") as product_outfile, \
+     open(review_output_file, "w", newline="", encoding="utf-8") as review_outfile:
+
+    # create CSV reader and writers
+    reader = csv.DictReader(infile)
+    product_writer = csv.writer(product_outfile)
+    review_writer = csv.writer(review_outfile)
+
+    # write headers to output files
+    product_writer.writerow(["product_number", "product_name", "price", "image_link", "color"])
+    review_writer.writerow(["product_number", "review_text"])
+
+    # iterate over input rows and write data to output files
+    product_number = 0
+    for row in reader:
+        # write product data to products.csv
+        product_writer.writerow([product_number, row["Naam"], row["Prijs"], row["Foto"], row["Kleur"]])
+
+        # write review data to reviews.csv
+        reviews = eval(row["Review"])
+        for review in reviews:
+            review_writer.writerow([product_number, review])
+
+        # increment product number for next row
+        product_number += 1
+
+print("Done!")
